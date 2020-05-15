143b6320a4120abbc406960535fcdeac5956f8d5
TremblingOdin <pigmekevin@gmail.com>
2020-05-14 20:44:04 -0400

    Began on shooter, new jump method broke sample 2D platformer, but not sample 2d swimmer

--- Assets/Scripts/Player/CombatSystem.cs
+++ Assets/Scripts/Player/CombatSystem.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class CombatSystem : MonoBehaviour
 {
+    private bool invincible;
+    private int inviFramecount;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private BoxCollider2D hurtBox;
+
+    protected List<KeyCode> attacks;
+
+    protected virtual void Awake()
     {
-        
+        attacks = new List<KeyCode>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(invincible && inviFramecount-- > 0)
+        {
+            hurtBox.enabled = false;
+        } else
+        {
+            invincible = false;
+            hurtBox.enabled = true;
+        }
     }
 
     public void SetInvinicbility(int frames)
     {
+        invincible = true;
+        inviFramecount = frames;
+    }
 
+    public virtual void LoadAttack(Dictionary<KeyCode, Player.UserInput> attackSettings)
+    {
+        return;
     }
 }
---
+++ Assets/Scripts/Player/Shooter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : CombatSystem
+{
+    [SerializeField]
+    private Transform firepoint;
+
+    [SerializeField]
+    private GameObject[] bulletPrefabs;
+
+    private GameObject bulletSelection;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        bulletSelection = bulletPrefabs[0];
+    }
+
+    void Update()
+    {
+        if(GetComponent<Movement>().FaceRight && firepoint.localPosition.x < 0)
+        {
+            firepoint.localPosition = new Vector2(-firepoint.localPosition.x, firepoint.localPosition.y);
+        }
+        else if(!GetComponent<Movement>().FaceRight && firepoint.localPosition.x > 0)
+        {
+            firepoint.localPosition = new Vector2(-firepoint.localPosition.x, firepoint.localPosition.y);
+        }
+
+        foreach (KeyCode key in attacks) {
+            if (Input.GetKeyDown(key))
+            {
+                Instantiate(bulletSelection);
+            }
+        }
+    }
+
+
+    public override void LoadAttack(Dictionary<KeyCode, Player.UserInput> attackSettings)
+    {
+        foreach(KeyCode key in attackSettings.Keys)
+        {
+            if(attackSettings[key] == Player.UserInput.ATTACK)
+            {
+                attacks.Add(key);
+            }
+        }
+    }
+}
